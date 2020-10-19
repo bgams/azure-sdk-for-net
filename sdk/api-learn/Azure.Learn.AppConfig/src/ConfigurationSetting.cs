fec767c3e83580dfc539d5958d8f5086aca3f6f3
Bo Gao <bga@microsoft.com>
2020-10-19 14:23:16 -0700

    Create public API

---
+++ sdk/api-learn/Azure.Learn.AppConfig/src/ConfigurationSetting.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Azure.Core;
+
+namespace Azure.Learn.AppConfig
+{
+    /// <summary>
+    /// Configuration settings model
+    /// </summary>
+    [CodeGenModel("KeyValue")]
+    public partial class ConfigurationSetting
+    {
+        /// <summary>
+        /// Etag
+        /// </summary>
+        [CodeGenMember("Etag")]
+        public ETag ETag { get; }
+    }
+}
